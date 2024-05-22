e581e80c0f0ee24789db3621800d1513a4b0f5b9
StephAdongo <stephanie@kabarak.ac.ke>
2024-05-22 16:32:58 +0300

    add .

---
+++ calc.js
@@ -0,0 +1,16 @@
+function addNumbers (num1, num2) {
+  return num1 + num2;
+}
+
+function subNumbers(num1, num2) {
+    return num1 - num2;
+}
+
+function multNumbers (num1,num2) {
+    return num1 * num2;
+}
+
+function divNumbers(num1, num2) {
+    return num2/num1;
+}
+
